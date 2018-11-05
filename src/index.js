fedaed174a1086ac44dbd23f117dfb69b6655873
Fabien Bernard <fabien.bernard@tabmo.io>
2018-11-05 21:55:45 +0100

    No longer use env.json static file

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import './main.css';
 import { Elm } from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
 
-const node = document.getElementById('root');
-
-fetch('env.json')
-  .then(response => response.json())
-  .then(flags => Elm.Main.init({ flags, node }))
-  .catch(() => node.innerText = 'Oops! Unreachable or invalid configuration.');
+Elm.Main.init({
+  node: document.getElementById('root'),
+  flags: {
+    lotteryUrl: 'http://localhost:8088'
+  }
+});
 
 registerServiceWorker();
